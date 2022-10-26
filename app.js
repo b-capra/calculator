3ceb24c33e40c0b1f9bc540ad9e5cd191aefa765
b-capra <caprabmc@gmail.com>
2022-10-26 12:53:58 -0400

    Clean up js for number inputs

--- app.js
+++ app.js
@@ -1,57 +1,13 @@
-const one = document.getElementById('one');
-const two = document.getElementById('two');
-const three = document.getElementById('three');
-const four = document.getElementById('four');
-const five = document.getElementById('five');
-const six = document.getElementById('six');
-const seven = document.getElementById('seven');
-const eight = document.getElementById('eight');
-const nine = document.getElementById('nine');
-const zero = document.getElementById('zero');
+const numbers = document.querySelectorAll('.number');
+
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        console.log(parseInt(number.textContent))
+    });
+});
 
 const vpText = document.getElementById('viewText');
 vpText.textContent = '';
 
 const history = document.getElementById('histText');
 history.textContent = '';
-
-
-one.addEventListener('click', () => {
-    console.log('1');
-});
-
-two.addEventListener('click', () => {
-    console.log('2');
-});
-
-three.addEventListener('click', () => {
-    console.log('3');
-});
-
-four.addEventListener('click', () => {
-    console.log('4');
-});
-
-five.addEventListener('click', () => {
-    console.log('5');
-});
-
-six.addEventListener('click', () => {
-    console.log('6');
-});
-
-seven.addEventListener('click', () => {
-    console.log('7');
-});
-
-eight.addEventListener('click', () => {
-    console.log('8');
-});
-
-nine.addEventListener('click', () => {
-    console.log('9');
-});
-
-zero.addEventListener('click', () => {
-    console.log('0');
-})
