278820726ad7ed934404acfc5c9dd72d045149eb
b-capra <caprabmc@gmail.com>
2022-11-02 23:56:00 -0400

    Add clear, equal function; change bg color, shadow

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+const mainText = document.getElementById('viewText');
+mainText.textContent = '';
+
+const histText = document.getElementById('histText');
+histText.textContent = '';
+
 const numbers = document.querySelectorAll('.number');
 
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
-        console.log(parseInt(number.textContent))
+        console.log(parseInt(number.textContent));
+        mainText.textContent += number.textContent;
     });
 });
 
-const vpText = document.getElementById('viewText');
-vpText.textContent = '';
+const clear = document.querySelector('#clear');
 
-const history = document.getElementById('histText');
-history.textContent = '';
+clear.addEventListener('click', () => {
+    mainText.textContent = '';
+    histText.textContent = '';
+});
+
+const equal = document.querySelector('.equal');
+
+equal.addEventListener('click', () => {
+    histText.textContent = mainText.textContent + " =";
+    mainText.textContent = "Answer"
+})
+
+const add = function() {
+
+}
